b6006046aa72ef2a790401eb081793367d8e566f
Amar <amarjha880@gmail.com>
2025-03-08 19:51:42 +0530

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+        <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
+        <p className='text-normal'>&copy; All Rights Reserved {new Date().getFullYear()}.</p>
+
+            <div className='flex items-center gap-4 justify-center text-2xl'>
+                <a href='' className='hover:text-blue-500'>
+                    <FaFacebook/>
+                </a>
+                <a href='' className='hover:text-blue-500'>
+                    <FaInstagram/>
+                </a>
+                <a href='' className='hover:text-blue-500'>
+                    <FaLinkedin/>
+                </a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
